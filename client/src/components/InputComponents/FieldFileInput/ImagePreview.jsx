f29f00587509549604f5fc3300b4f1dfc9a059c8
Mikhail Shvets <shvetsm123@gmail.com>
2023-11-02 15:16:47 +0200

    minor fixes

--- client/src/components/InputComponents/FieldFileInput/ImagePreview.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const ImagePreview = ({ src }) => <img src={src} alt="Preview" />;
-
-export default ImagePreview;
--- client/src/components/InputComponents/FieldFileInput/PdfPreview.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const PdfPreview = ({ src }) => (
-  <object data={src} type="application/pdf"></object>
-);
-
-export default PdfPreview;
