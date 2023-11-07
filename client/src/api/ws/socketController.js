1bee4da2ae70fdc39f09f698b8c8591d1cca0fdc
Mikhail Shvets <shvetsm123@gmail.com>
2023-11-07 23:34:45 +0200

    minor bugs fixes

--- client/src/api/ws/socketController.js
+++ client/src/api/ws/socketController.js
@@ -6,7 +6,7 @@ import NotificationSocket from './sockets/NotificationSocket';
 export let controller;
 export let chatController;
 
-export const initSocket = store => {
+export const initSocket = (store) => {
   controller = new NotificationSocket(
     store.dispatch,
     store.getState,
