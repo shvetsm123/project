871707b5e5791580fd69cbd2ee3dd9d779af9bf7
Mikhail Shvets <shvetsm123@gmail.com>
2023-11-22 13:06:57 +0200

    sql cashback

--- server/todo-dev.session.sql
+++ server/todo-dev.session.sql
@@ -1,8 +1,40 @@
+// прочее
 UPDATE "Banks"
 SET expiry = '09/30'
 WHERE name = 'yriy';
+
+// подсчитать всех юзеров по ролям - 9 таска
 
 SELECT role, COUNT(*) AS user_count
 FROM "Users"
 WHERE role IN ('creator', 'customer')
 GROUP BY role;
+
+// добавил всем кастомерам +10 на баланс = 10 таска
+
+UPDATE "Users"
+SET "balance" = "balance" + 10
+WHERE "role" = 'customer';
+
+// вывел баланс + кешбек кастомеров - 10 таска
+
+SELECT
+  "id",
+  "firstName",
+  "lastName",
+  "displayName",
+  "balance",
+  ("balance" * 0.1 * COALESCE((SELECT COUNT(*) FROM "Contests" WHERE "userId" = "Users"."id" AND "createdAt" BETWEEN '2023-11-01' AND '2024-01-14'), 0)) AS "cashback"
+FROM
+  "Users"
+WHERE
+  "role" = 'customer';
+
+// апдейт баланса (не выполнял) - 10 таска
+
+UPDATE "Users"
+SET "balance" = "balance" + ("balance" * 0.1 * COALESCE((SELECT COUNT(*) FROM "Contests" WHERE "userId" = "Users"."id" AND "createdAt" BETWEEN '2023-12-25' AND '2024-01-14'), 0));
+WHERE "role" = 'customer';
+
+
+
