1bbc46f42a243a66b2d12c8dfc706718212c624e
Mikhail Shvets <shvetsm123@gmail.com>
2023-11-27 22:36:53 +0200

    DO NOT COMMIT IT

--- server/src/seeders/123123123-Bank.js
+++ server/src/seeders/123123123-Bank.js
@@ -1,21 +1,27 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Banks', [
-      {
-        cardNumber: '4564654564564564',
-        name: 'SquadHelp',
-        expiry: '11/22',
-        cvc: '453',
-        balance: 0,
-      },
-      {
-        cardNumber: '4111111111111111',
-        name: 'yriy',
-        expiry: '09/23',
-        cvc: '505',
-        balance: 5000,
-      },
-    ], {});
+    return queryInterface.bulkInsert(
+      'Banks',
+      [
+        {
+          cardNumber: '4564654564564564',
+          name: 'SquadHelp',
+          expiry: '11/22',
+          cvc: '453',
+          balance: 0,
+        },
+        {
+          cardNumber: '4111111111111111',
+          name: 'yriy',
+          expiry: '09/23',
+          cvc: '505',
+          balance: 5000,
+        },
+      ],
+      {}
+    );
   },
-
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Selects', null, {});
+  },
 };
